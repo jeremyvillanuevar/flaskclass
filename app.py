8edb2ed4f1f291891de797d52bc5910dd0ceff08
jeremyvillanuevar <78555867+jeremyvillanuevar@users.noreply.github.com>
2024-02-22 12:45:33 +0000

    Clase 2

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
+def index():
+    print(app.url_for('give_greeting', name='Mark'))
+
+@app.route('/helloworld')
 def hello_world():
     return "Hello world"
 
 @app.route('/routed')
 def hello_world_otraruta():
     return "Hello world de otra ruta"
+
+@app.route('/temp')
+def hello_world_temp():
+    lista ={1,2,3,4,5}
+    return render_template("ejemplo.html",nombre="Jeremy",lista=lista)
+
+@app.route("/hello/<username>")
+def hello_user(username):
+    print(app.url_for('hello_world_otraruta', name='Mark'))
+    return "Hello {} !".format(username)
+
+@app.route('/greeting/<name>')
+def give_greeting(name):
+    return 'Hello, {0}!'.format(name)
 
 if __name__=="__main__":
     app.run(debug=True)
--- notflask.py
+++ notflask.py
@@ -1,4 +1,33 @@
 class NotFlask():
-    pass
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, route_str):
+        def decorator(f):
+            self.routes[route_str] = f
+            return f
+
+        return decorator
+
+    def serve(self, path):
+        view_function = self.routes.get(path)
+        if view_function:
+            return view_function()
+        else:
+            raise ValueError('Route "{}"" has not been registered'.format(path))
+
 
 app = NotFlask()
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/routed')
+def hello_world_otraruta():
+    return "Hello world de otra ruta"
+
+    
+print(app.serve("/"))
+print(app.serve("/routed"))
+print(app.serve("/route"))
